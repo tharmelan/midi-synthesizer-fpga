59497f628b861c7cd29e640706128c4bc8a88298
tharmelan <tharmelan@hotmail.com>
2020-06-12 17:47:20 +0200

    Update lcd_ui.vhd

--- source/MS5_LCD/lcd_ui.vhd
+++ source/MS5_LCD/lcd_ui.vhd
@@ -1,4 +1,18 @@
 -------------------------------------------
+-- ZHAW School of Engineering
+-- Technikumstrasse 9
+-- 8401 Winterthur
+--
+-- Autoren:
+-- Beat Sturzenegger
+-- Markus Bodenmann
+-- Tharmelan Theivanesan
+--
+-- source name: lcd_ui
+-- Datum:       14.06.2020
+-------------------------------------------
+
+-------------------------------------------
 -- Block code:  lcd_ui.vhd
 -- Function:
 -------------------------------------------
