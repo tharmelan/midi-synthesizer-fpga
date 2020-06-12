c6d32f0f10590a0286f16140db24fa80a522ea29
tharmelan <tharmelan@hotmail.com>
2020-06-12 17:37:44 +0200

    Update baud_tick.vhd

--- source/MS4_MIDI/baud_tick.vhd
+++ source/MS4_MIDI/baud_tick.vhd
@@ -1,4 +1,18 @@
 -------------------------------------------
+-- ZHAW School of Engineering
+-- Technikumstrasse 9
+-- 8401 Winterthur
+--
+-- Autoren:
+-- Beat Sturzenegger
+-- Markus Bodenmann
+-- Tharmelan Theivanesan
+--
+-- source name: baud_tick
+-- Datum:       14.06.2020
+-------------------------------------------
+
+-------------------------------------------
 -- Block code:  baud_tick.vhd
 -- Function: Generates a tick in the middle of each bit of a UART signal
 -------------------------------------------
