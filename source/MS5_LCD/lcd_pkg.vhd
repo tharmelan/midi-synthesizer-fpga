d909012503faae1f679d76e1c87fb7381eb57caf
tharmelan <tharmelan@hotmail.com>
2020-06-12 17:46:18 +0200

    Update lcd_pkg.vhd

--- source/MS5_LCD/lcd_pkg.vhd
+++ source/MS5_LCD/lcd_pkg.vhd
@@ -1,4 +1,18 @@
 -------------------------------------------
+-- ZHAW School of Engineering
+-- Technikumstrasse 9
+-- 8401 Winterthur
+--
+-- Autoren:
+-- Beat Sturzenegger
+-- Markus Bodenmann
+-- Tharmelan Theivanesan
+--
+-- source name: lcd_pkg
+-- Datum:       14.06.2020
+-------------------------------------------
+
+-------------------------------------------
 -- Block code:  lcd_pkg.vhd
 -- Function:
 -------------------------------------------
